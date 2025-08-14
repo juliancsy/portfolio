4600095a4f2a0845337c3e2038734b5440670b8b
Julian Sy <js@chengj.in>
2025-08-13 19:51:38 -0700

    config update

--- src/consts.ts
+++ src/consts.ts
@@ -1,24 +1,28 @@
 import type { IconMap, SocialLink, Site } from '@/types'
 
 export const SITE: Site = {
-  title: 'astro-erudite',
+  title: 'Julian Chengjin Sy',
   description:
-    'astro-erudite is a opinionated, unstyled blogging template—built with Astro, Tailwind, and shadcn/ui.',
-  href: 'https://astro-erudite.vercel.app',
-  author: 'jktrn',
+    "Julian Chengjin Sy's UX Engineer portfolio",
+  href: 'https://chengj.in',
+  author: 'juliancsy',
   locale: 'en-US',
   featuredPostCount: 2,
   postsPerPage: 3,
 }
 
 export const NAV_LINKS: SocialLink[] = [
+  // {
+  //   href: '/blog',
+  //   label: 'blog',
+  // },
+  // {
+  //   href: '/authors',
+  //   label: 'authors',
+  // },
   {
-    href: '/blog',
-    label: 'blog',
-  },
-  {
-    href: '/authors',
-    label: 'authors',
+    href: '/case',
+    label: 'case studies',
   },
   {
     href: '/about',
@@ -28,21 +32,21 @@ export const NAV_LINKS: SocialLink[] = [
 
 export const SOCIAL_LINKS: SocialLink[] = [
   {
-    href: 'https://github.com/jktrn',
+    href: 'https://github.com/juliancsy',
     label: 'GitHub',
   },
   {
-    href: 'https://twitter.com/enscry',
-    label: 'Twitter',
+    href: 'https://www.linkedin.com/in/juliancsy/',
+    label: 'LinkedIn',
   },
   {
-    href: 'mailto:jason@enscribe.dev',
+    href: 'mailto:js@chengj.in',
     label: 'Email',
   },
-  {
-    href: '/rss.xml',
-    label: 'RSS',
-  },
+  // {
+  //   href: '/rss.xml',
+  //   label: 'RSS',
+  // },
 ]
 
 export const ICON_MAP: IconMap = {
